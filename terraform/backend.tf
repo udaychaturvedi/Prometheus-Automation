8bd2ccb4d92c2b429f4bec92dd92ce57ce983020
udaychaturvedi <udaysharma5445@gmail.com>
2025-11-22 06:35:29 +0530

    Update VPC, subnet, SG, backend for new account

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "prometheus-terraform-state-uday123-new"
     key            = "prometheus/terraform.tfstate"
-    region         = "ap-south-1"
+    region         = "eu-north-1"
     dynamodb_table = "terraform-locks"
     encrypt        = true
   }
